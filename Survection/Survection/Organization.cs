85082c6e3be105d3625500bbc497909cb255bfb7
Sakshi Monga <saksh@microsoft.com>
2018-08-29 17:17:06 +0530

    Name change of edmx file

--- Survection/Survection/Organization.cs
+++ Survection/Survection/Organization.cs
@@ -12,18 +12,19 @@ namespace Survection
     using System;
     using System.Collections.Generic;
     
-    public partial class Category
+    public partial class Organization
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Category()
+        public Organization()
         {
-            this.QuestionBanks = new HashSet<QuestionBank>();
+            this.Surveys = new HashSet<Survey>();
         }
     
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
+        public int OrgId { get; set; }
+        public string OrgName { get; set; }
+        public Nullable<int> AdminId { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<QuestionBank> QuestionBanks { get; set; }
+        public virtual ICollection<Survey> Surveys { get; set; }
     }
 }
