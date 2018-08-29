b5df423b1b2098239f8def298365b133b88c5150
Shubhankar Nath <shnath@microsoft.com>
2018-08-29 14:37:13 +0530

    Initial Commit.

---
+++ Survection/Survection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Survection.Startup))]
+namespace Survection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
